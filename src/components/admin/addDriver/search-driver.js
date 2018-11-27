56e06259f3e11503b614bf3b5ec118f0042b7c91
lakhwinder-aika <lakhwinder@aika.digital>
2018-11-27 19:18:07 +0530

    driver Id on list

--- src/components/admin/addDriver/search-driver.js
+++ src/components/admin/addDriver/search-driver.js
@@ -220,9 +220,6 @@ class SearchDriver extends Component {
                                           </span>
                                         </span>
                                       ) : null}
-                                     
-                                    </div>
-                                    <div className="list-info-other col-sm-6">
                                       {driver.postal_code ? (
                                         <span>
                                           <span className="info-label">Zip: </span>{' '}
@@ -231,7 +228,17 @@ class SearchDriver extends Component {
                                           </span>
                                         </span>
                                       ) : null}
-                                      
+                                     
+                                    </div>
+                                    <div className="list-info-other col-sm-6">
+                                      {driver.ref_id ? (
+                                        <span>
+                                          <span className="info-label">Driver Id: </span>{' '}
+                                          <span className="info-txt">
+                                            {driver.ref_id}
+                                          </span>
+                                        </span>
+                                      ) : null}
                                       {driver.email ? (
                                         <span>
                                           <span className="info-label">Email:</span>{' '}
